5349887941c7b829dac26723cb7943b838485f9c
Anthony Dahanne <anthony.dahanne@gmail.com>
2016-01-28 15:46:03 -0500

    using Docker java to interact with the Docker daemon

--- README.md
+++ README.md
@@ -2,6 +2,10 @@ To build (java & dockerfile) :
 
     $ mvn clean package docker:build
 
+To run :
+
+    $ docker run -p 8080:8080 --link socat:socat --name=demo -d  anthonydahanne/demo --dockerUrl=http://socat:2375
+
 and push to the dockerhub registry :
 
     $ mvn clean package docker:build -DpushImage
@@ -11,3 +15,4 @@ Dockerfile info : [https://spring.io/guides/gs/spring-boot-docker/](https://spri
 
 Docker Maven plugin : [https://github.com/spotify/docker-maven-plugin](https://github.com/spotify/docker-maven-plugin)
 
+Docker Java : [https://github.com/docker-java/docker-java/wiki](https://github.com/docker-java/docker-java/wiki)
--- src/main/java/net/dahanne/GreetingController.java
+++ src/main/java/net/dahanne/GreetingController.java
@@ -1,16 +1,41 @@
 package net.dahanne;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.InfoCmd;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.api.model.Info;
+import com.github.dockerjava.core.DockerClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class GreetingController {
+
+    @Value("${dockerUrl}")
+    private String dockerUrl;
 
     @RequestMapping("/")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+
+        DockerClient dockerClient = DockerClientBuilder.getInstance(dockerUrl).build();
+        Info info = dockerClient.infoCmd().exec();
+
+
+        List<Container> containers = dockerClient.listContainersCmd().withShowAll(false).exec();
+
+
+
+        model.addAttribute("info", info);
+
+
+        model.addAttribute("containers", containers);
+
         return "greeting";
     }
 
